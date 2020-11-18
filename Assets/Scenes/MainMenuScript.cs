dc292074a0ac351347b617c4c6bcbb6e16a512e9
Aki Tervo <tervo.aki@gmail.com>
2020-11-18 16:02:53 +0200

    Initial Commit
    Main menu scene and a script file for it added

---
+++ Assets/Scenes/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartFirstMiniGame()
+    {
+        print(1);
+    }
+
+    public void StartSecondMiniGame()
+    {
+        print(2);
+    }
+
+    public void StartThirdMiniGame()
+    {
+        print(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
