9c6e68eebcbf592ab67338e9bf161b1d4fcf962b
Janne Saarinen <janne.saarinen@mail.com>
2020-12-07 13:25:34 +0200

    lisätty kolmas interaktiivinen kuva

---
+++ Assets/Scripts/InteractiveImagesScripts/TranslaatioUAC.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TranslaatioUAC : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objectPlace;
+
+    private Vector2 initialPosition;
+
+    private Vector2 mousePosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked;
+
+    // Start is called before the first frame update
+    void Start() {
+        initialPosition = transform.position;
+  
+    }
+
+    private void OnMouseDown() {
+        if (!locked)
+        {
+            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag() {
+        if (!locked)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
+        }
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+    
+//        SceneManager.LoadScene("DNAScene");
+        
+//        Destroy(GameObject.Find("Text"));
+//        Destroy(GameObject.Find("Text (1)"));
+//        DontDestroyOnLoad(GameObject.Find("Canvas"));
+//        GameObject go = GameObject.Find("Text");
+//        go.transform.position = new Vector3(700,100,0);
+//        go.GetComponent<Text>().text = "Rakenna RNA";
+            
+
+            
+        }
+
+    private void OnMouseUp() {
+        if (Mathf.Abs(transform.position.x - objectPlace.position.x) <= 0.5f &&
+            Mathf.Abs(transform.position.y - objectPlace.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(objectPlace.position.x, objectPlace.position.y);
+            locked = true;
+            
+            GameObject go = GameObject.Find("PlaceholderUAC");
+            go.transform.position = new Vector3(-100f,-100f,0);
+
+            GameObject go2 = GameObject.Find("PlaceholderUGA");
+            go2.transform.position = new Vector3(gameObject.transform.position.x + 1.5f,
+            gameObject.transform.position.y, 0);
+            
+            StartCoroutine(ExecuteAfterTime(1));
+            
+        }
+        else
+        {
+            transform.position = new Vector2(initialPosition.x, initialPosition.y);
+        }
+    }
+}
