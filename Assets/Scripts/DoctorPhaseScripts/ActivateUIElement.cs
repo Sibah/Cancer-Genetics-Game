78410a50944ac64ca29461b8c4c33a78801dc82f
Aki Tervo <tervo.aki@gmail.com>
2021-02-17 15:26:41 +0200

    Created Buttons for doctor phase content

---
+++ Assets/Scripts/DoctorPhaseScripts/ActivateUIElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateUIElement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ActivatableUIElement;
+
+    public void ActivateElement()
+    {
+        ActivatableUIElement.SetActive(true);
+    }
+}
