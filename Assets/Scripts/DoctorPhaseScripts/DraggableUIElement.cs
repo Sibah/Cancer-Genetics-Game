7b8e7d76fc5b90d75d842114591c754e4b325a77
Aki Tervo <tervo.aki@gmail.com>
2021-02-18 00:08:24 +0200

    Improved the buttons, made draggable UI elements

---
+++ Assets/Scripts/DoctorPhaseScripts/DraggableUIElement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableUIElement : EventTrigger
+{
+    public bool isDragging;
+    public DragTarget dragTarget;
+
+    void Start() 
+    {
+        dragTarget = GetComponentInParent<DragTarget>();    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            dragTarget.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        print("start");
+        isDragging = true;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        print(eventData);
+        isDragging = false;
+    }
+}
