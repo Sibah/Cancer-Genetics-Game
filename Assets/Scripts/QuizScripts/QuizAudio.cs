1c39909a462f04244b41999b3e334f55a15ebb0d
Rasmus Kyyhkynen <rojkyy@utu.fi>
2021-02-23 19:06:29 +0200

    quiz game music

--- Assets/Scripts/QuizScripts/QuizAudio.cs
+++ Assets/Scripts/QuizScripts/QuizAudio.cs
@@ -13,11 +13,11 @@ public class QuizAudio : MonoBehaviour
 
     public void CorrectClick()
     {
-        source.PlayOneShot(correctClick, 0.1f);
+        source.PlayOneShot(correctClick, 0.5f);
     }
 
     public void InCorrectClick()
     {
-        source.PlayOneShot(inCorrectClick, 0.1f);
+        source.PlayOneShot(inCorrectClick, 1.5f);
     }
 }
