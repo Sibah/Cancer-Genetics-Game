a9a1d6ca455e8981784fca3df20f92485f0be794
Aki Tervo <tervo.aki@gmail.com>
2021-03-04 15:11:15 +0200

    Both patient cases are now added to the doctor phase

--- Assets/Scripts/DoctorPhaseScripts/DoctorPhaseManager.cs
+++ Assets/Scripts/DoctorPhaseScripts/DoctorPhaseManager.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class DoctorPhaseManager : MonoBehaviour
 {
+    public GameObject[] cases;
     public GameObject[] phases;
+    public Transform currentlyActiveCase;
+
+    void Start() 
+    {
+        int caseIndex = Random.Range(0, cases.Length);
+        currentlyActiveCase = cases[caseIndex].transform; 
+        cases[caseIndex].SetActive(true);
+        GatherPhases(caseIndex);
+    }
 
     public void ActivatePhase(int index)
     {
@@ -15,6 +25,29 @@ public class DoctorPhaseManager : MonoBehaviour
 
         DeactivatePhases();
         phases[index].SetActive(true);
+    }
+
+    private void GatherPhases(int index)
+    {
+        if(index >= cases.Length)
+        {
+            index = cases.Length-1;
+        }
+
+        if(index < 0)
+        {
+            index = 0;
+        }
+        
+        Transform currentCase = cases[index].transform;
+        GameObject[] currentPhases = new GameObject[currentCase.childCount];
+
+        for(int i = 0; i < currentCase.childCount; i++)
+        {
+            currentPhases[i] = currentCase.GetChild(i).gameObject;
+        }
+
+        phases = currentPhases;
     }
 
     private void DeactivatePhases()
