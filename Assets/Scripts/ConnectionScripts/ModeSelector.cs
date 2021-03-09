f79eec2e692f2a83153d4920c05c59d21b394fc3
Aki Tervo <tervo.aki@gmail.com>
2021-03-09 22:33:04 +0200

    Added more words, Players should now be able to choose amount of rounds and the topic

--- Assets/Scripts/ConnectionScripts/ModeSelector.cs
+++ Assets/Scripts/ConnectionScripts/ModeSelector.cs
@@ -8,8 +8,75 @@ public class ModeSelector : MonoBehaviour
     public GameObject startScreen;
     public GameObject connectionMode;
     public GameObject resultScreen;
+    public GameObject dataObjects;
+    public GameObject startPhase1;
+    public GameObject startPhase2;
+    public GameObject startPhase3;
     public WordLineInitializer wordInit;
     public WordDatabase currentDatabase;
+
+    public void SelectBasisOfGenetics()
+    {
+        GoToPhase1();
+        currentDatabase = dataObjects.transform.GetChild(1).GetComponent<WordDatabase>();
+        SelectConnectionMode();
+    }
+
+    public void SelectCancerGenetics()
+    {
+        GoToPhase1();
+        currentDatabase = dataObjects.transform.GetChild(0).GetComponent<WordDatabase>();
+        SelectConnectionMode();
+    }
+
+    public void GoToPhase1()
+    {
+        startPhase1.SetActive(true);
+        startPhase2.SetActive(false);
+        startPhase3.SetActive(false);
+    }
+
+    public void GoToPhase2()
+    {
+        startPhase1.SetActive(false);
+        startPhase2.SetActive(true);
+        startPhase3.SetActive(false);
+    }
+
+    public void GoToPhase3()
+    {
+        startPhase1.SetActive(false);
+        startPhase2.SetActive(false);
+        startPhase3.SetActive(true);
+    }
+
+    public void SelectRoundAmount(int amount)
+    {
+        switch(amount)
+        {
+            case 0:
+                return;
+            case 1:
+                wordInit.roundAmount = 0;
+                break;
+            case 2:
+                wordInit.roundAmount = 2;
+                break;
+            case 3:
+                wordInit.roundAmount = 3;
+                break;
+            case 4: 
+                wordInit.roundAmount = 4;
+                break;
+            case 5:
+                wordInit.roundAmount = 5;
+                break;
+            default:
+                Debug.LogError($"Unknown value given from dropdown field in start screen phase 2: {amount}");
+                break;
+        }
+        GoToPhase3();
+    }
 
     public void SelectConnectionMode()
     {
