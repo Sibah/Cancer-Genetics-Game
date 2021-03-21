083ada25008c07b7525e16467177637727b4ee10
Aki Tervo <tervo.aki@gmail.com>
2021-03-21 11:57:10 +0200

    Reverted last change

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -13,10 +13,6 @@ public class MainMenuController : MonoBehaviour
     {
         SetSoundState();
         DontDestroyOnLoad(this.gameObject);
-        if(FindObjectsOfType<MainMenuController>().Length > 1)
-        {
-            Destroy(this.gameObject);
-        }
     }
 
     public void StartQuizGame()
